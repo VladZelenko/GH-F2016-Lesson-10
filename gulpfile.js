3dbd0ded8ca5a874e24f0127b043df1fd7bd5c5d
Vlad6555 <Vlad6555@ukr.net>
2016-12-24 19:54:18 +0200

    preparation for unloading

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
 		sass = require('gulp-sass'),
-		browserSync = require('browser-sync');
+		browserSync = require('browser-sync'),
+		del = require('del');
 
 gulp.task("sass", function() {
 	return gulp.src('app/sass/**/*.scss')
@@ -18,9 +19,29 @@ gulp.task('browser-sync', function() {
 	});
 });
 
+gulp.task('clean', function() {
+	return del.sync('dist');
+});
+
 
 gulp.task('watch', ['browser-sync', 'sass'], function() {
     gulp.watch('app/sass/**/*.scss', ['sass']);
     gulp.watch('app/**/*.html', browserSync.reload);
     gulp.watch('app/js/**/*.js', browserSync.reload);
+});
+
+
+gulp.task('build', ['clean', 'sass'], function() {
+
+	var buildCss = gulp.src([
+		'app/css/main.css'
+		])
+	.pipe(gulp.dest('dist/css'))
+
+	var buildJs = gulp.src('app/js/**/*')
+	.pipe(gulp.dest('dist/js'))
+
+	var buildHtml = gulp.src('app/*.html')
+	.pipe(gulp.dest('dist'));
+
 });
